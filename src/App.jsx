48132dae9a3c2c9b413f86324077a3d8d65b7ea7
Abrham <abrsh6266@gmail.com>
2024-02-18 22:53:08 -0900

    adding toastify library

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import ColorList from "./ColorList";
 import Form from "./Form";
 import Values from "values.js";
 import { ToastContainer, toast } from "react-toastify";
-toast.success("awesome");
-toast.error("error message");
 
 const App = () => {
   const [color, setColor] = useState("");
   const [colors, setColors] = useState(new Values("#b00c0c").all(10));
   const handleColor = () => {
-    setColors(new Values(color).all(10));
+    try {
+      setColors(new Values(color).all(10));
+      toast.success('')
+    } catch (error) {
+      toast.error(error.message)
+    }
   };
   return (
     <main>
       <Form color={color} setColor={setColor} handleColor={handleColor} />
       <ColorList colors={colors} />
-      {/* <ToastContainer position="top-center" /> */}
+      <ToastContainer position="top-center" />
     </main>
   );
 };
